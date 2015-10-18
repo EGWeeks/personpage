86c00a177cba0aa3c0f0d227d5f801b95bb3a685
Ethan Weeks <ethanweeks@ymail.com>
2015-10-18 15:08:22 -0600

    first commit basic design

---
+++ home.js
@@ -0,0 +1,12 @@
+//$(window).load(function){
+//	var allimages = $(".imagegallery");
+//}
+
+//<div class="imageGallery">
+//		<div class="image1">
+//			<img src="background.jpg" height=""width="500" alt="backcountry touring">
+//		</div>
+//		<div class="image2">
+//			<img src="harbor.jpg" height="400"width = "500" alt="maine harbor">
+//		</div>
+//	</div>	
