33b60c38433b46f2c1cdf38ff542587d9a5580fd
Ethan Weeks <ethanweeks@ymail.com>
2015-10-18 18:07:49 -0600

    adding images to html and ststarted gallery css/js needed

--- home.js
+++ home.js
@@ -1,12 +1,9 @@
-//$(window).load(function){
-//	var allimages = $(".imagegallery");
-//}
+//Goal: slideshow background images
+//Solution: Create underlay image 
 
-//<div class="imageGallery">
-//		<div class="image1">
-//			<img src="background.jpg" height=""width="500" alt="backcountry touring">
-//		</div>
-//		<div class="image2">
-//			<img src="harbor.jpg" height="400"width = "500" alt="maine harbor">
-//		</div>
-//	</div>	
+
+$(window).load(function){
+	var allimages = $(".imagegallery");
+}
+
+	
