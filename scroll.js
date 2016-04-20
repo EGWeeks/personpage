b9492b55690e682895325f266ea4ce86baf41b63
Ethan Weeks <ethanweeks@ymail.com>
2016-04-20 13:10:43 -0600

    removed js files + three columns on index

--- scroll.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	// mouse Trail
-	var $mouseT = $("#trail");
-	$(document).mousemove(function(event) {
-		$mouseT.css({left: (event.pageX + 3), top: (event.pageY + 15)});
-	});
-});
