d02f35aba52a4060180597beba82f42ba300fa83
Ethan Weeks <ethanweeks@ymail.com>
2016-04-25 12:18:23 -0600

    add selfie js/hover effect on self for description

---
+++ js/selfie.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function(){
+
+	$('.selfie').qtip({
+		content: 'Mid Poly Art - Made with Adobe Illustrator',
+		position: {
+			my: 'left center',
+			at: 'right center',
+			target: $('.selfie')
+		}
+	});
+
+});
