c7118a4bfcf839ed4a550838b8ac3d1516d94112
Ethan Weeks <ethanweeks@ymail.com>
2016-08-25 15:12:08 -0600

    updated project info and timeline sizing

--- js/services/projects.js
+++ js/services/projects.js
@@ -9,15 +9,15 @@ angular.module('projectsSrc', [])
 			return [
 				{
 					title: 'Pro Motocross API',
-					body: 'Built following a RESTful design pattern Express.js is serving up the latest lap times from the Lucas oil pro motocross series. Storing all data in a MongoDB hosted on mLab for easy JSON returns.',
+					body: 'Built following a RESTful design pattern Express.js is serving up the latest lap times from the Lucas oil pro motocross series. Storing all data in a MongoDB hosted on mLab for easy JSON returns. The build inspiration came from watching the races every weekend and the announcers talking about how riders pace develop over the course of the race and I thought, "I need to graph that out". Come to find out there is no motocross API, so I filled the void.',
 					tech: 'Node.js, Heroku, MongoDB, mLab',
 					classes: 'arrow-right',
 					url: 'http://promotocrossapi.com',
 					githubURL: 'https://github.com/EGWeeks/promotocrossapi'
 				},
 				{
-					title: 'BackWoods Contact',
-					body: 'Outdoor enthusiast app that was built to showcase my ability of developing on all layers of the stack. ',
+					title: 'Backwoods Contact',
+					body: 'Backwoods is an outdoor sporting classifieds app that I built for my love of getting outside. Backwoods uses JSON Web Tokens for authorization, knex.js to query the Postgres database, express.js for routing, Angular for all the front end funcationality.',
 					tech: 'AngularJS, Google Maps API, PostgreSQL, Express.js',
 					classes: 'arrow-right',
 					url: 'https://backwoodscontact.xyz/',
@@ -39,7 +39,7 @@ angular.module('projectsSrc', [])
 				{
 					title: 'Weather Map',
 					body: 'The parser was the first step of creating the motocross API because the organization that host the data only offers it in PDFs. Using NPM, ES6, and Promises it is downloading all the PDFs, parsing them to JSON format, and then pushing to my hosted MongoDB.',
-					tech: 'jQuery, Foundation, Google Maps API, Wunderground API',
+					tech: 'jQuery, Google Maps, Wunderground',
 					classes: 'arrow-left',
 					url: 'https://weathermap.firebaseapp.com',
 					githubURL: 'https://github.com/EGWeeks/WeatherMap'
