0e3cf74dee79f9b7471f835b44b2adc569abf3d9
Ethan Weeks <ethanweeks@ymail.com>
2016-08-28 14:03:49 -0600

    updated project services weather map description

--- js/services/projects.js
+++ js/services/projects.js
@@ -11,7 +11,6 @@ angular.module('projectsSrc', [])
 					title: 'Pro Motocross API',
 					body: 'Built following a RESTful design pattern Express.js is serving up the latest lap times from the Lucas oil pro motocross series. Storing all data in a MongoDB hosted on mLab for easy JSON returns. The build inspiration came from watching the races every weekend and the announcers talking about how riders pace develop over the course of the race and I thought, "I need to graph that out". Come to find out there is no motocross API, so I filled the void.',
 					tech: 'Node.js, Heroku, MongoDB, mLab',
-					classes: 'arrow-right',
 					url: 'http://promotocrossapi.com',
 					githubURL: 'https://github.com/EGWeeks/promotocrossapi'
 				},
@@ -19,7 +18,6 @@ angular.module('projectsSrc', [])
 					title: 'Backwoods Contact',
 					body: 'Backwoods is an outdoor sporting classifieds app that I built for my love of getting outside. Backwoods uses JSON Web Tokens for authorization, knex.js to query the Postgres database, express.js for routing, Angular for all the front end funcationality.',
 					tech: 'AngularJS, Google Maps API, PostgreSQL, Express.js',
-					classes: 'arrow-right',
 					url: 'https://backwoodscontact.xyz/',
 					githubURL: 'https://github.com/EGWeeks/tutorFront'
 				}
@@ -32,15 +30,13 @@ angular.module('projectsSrc', [])
 					title: 'PDF parser into DB',
 					body: 'The parser was the first step of creating the motocross API because the organization that host the data only offers it in PDFs. Using NPM, ES6, and Promises it is downloading all the PDFs, parsing them to JSON format, and then pushing to my hosted MongoDB.',
 					tech: 'ES6, Node.js, NPM',
-					classes: 'arrow-left',
 					url: 'https://github.com/EGWeeks/AMAPDFtoJSONParser',
 					githubURL: 'https://github.com/EGWeeks/AMAPDFtoJSONParser'
 				},
 				{
 					title: 'Weather Map',
-					body: 'Weather map',
-					tech: 'jQuery, Google Maps, Wunderground',
-					classes: 'arrow-left',
+					body: 'Weather map uses the Wunderground API and the user\'s current location to return realtime weather data. I used ChartJS for hourly data visuals and Foundation\'s grid system to keep it organized. Since it is built on Google maps the user has the abilities to move the marker and get that coordinates weather information.',
+					tech: 'jQuery, Foundation, ChartJS, Wunderground',
 					url: 'https://weathermap.firebaseapp.com',
 					githubURL: 'https://github.com/EGWeeks/WeatherMap'
 				}
